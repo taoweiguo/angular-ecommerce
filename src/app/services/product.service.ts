b330036fb1b955df84fb8574ff31448f2f871dca
Taowei Guo <a953522511@gmail.com>
2024-04-02 22:18:17 -0400

    Commit all front end first time

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Product } from '../common/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  private baseUrl = 'http://localhost:8080/api/products'
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList(theCategoryId: number): Observable<Product[]> {
+    
+    // @TODO: need to build URL based on category id ...
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    console.log(this.httpClient.get<GetResponse>(searchUrl));
+
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+      map(response => response._embedded.products)
+    )
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
+}
